926318e9f93a3a56d8ac7d5a9bca01f66c9c2a52
Abdulrahman Mohammed <93239775+Abdulrahman48811@users.noreply.github.com>
2021-12-02 20:26:08 -0500

    Update breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,18 +2,14 @@ let breed = process.argv[2];
 const request = require('request');
 
 let response = request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, response, body) => {
-  // if no ERRORS
   if (!err) {
     let result = JSON.parse(body);
-
-    // if results EMPTY
     if (result.length === 0) {
       console.log('this breed is not found');
       // if results found
     } else {
       console.log(result[0].description);
     }
-    // if ERROR
   } else {
     console.log(err);
   }
